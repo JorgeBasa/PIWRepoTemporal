3b76d775edb0ed3c35f2e5a1668cbc406d2933ba
AaronFabrizio185686 <aaronfabriziogomez@outlook.com>
2024-11-05 18:10:03 -0700

    NEW Index.html

---
+++ Navbar.php
@@ -0,0 +1,17 @@
+<nav class="navbar">
+    <div class="logo">
+        <a href="#">Julia</a>
+    </div>
+    
+    <ul class="nav-links">
+        <li><a href="Index.html">Pagina Principal</a></li>
+        <li><a href="Libreria.html">Libreria</a></li>
+        <li><a href="login.html">Acceder</a></li>
+    </ul>
+    
+    <div class="burger-menu">
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+    </div>
+</nav>
