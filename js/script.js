4800d3e6496fc6be8ea6d843405fa00f6b0a90ba
Squalood <adriancas98@gmail.com>
2024-11-07 18:28:30 -0700

    add mejorado

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@ function toggleSidebar() {
     const sidebar = document.getElementById('sidebar');
     sidebar.classList.toggle('show');
 }
+
+function toggleAdd() {
+    const addcard = document.getElementById('addcard');
+    addcard.classList.toggle('show');
+
+    const overlay = document.getElementById('overlay');
+    overlay.classList.toggle('show');
+}
